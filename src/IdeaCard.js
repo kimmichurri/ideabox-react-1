87a09c0613a60833ed7e7443a786496e203c2f07
“Kimberly <kimberly.achkio@gmail.com>
2019-03-10 13:21:55 -0600

    Pass idea card component into render

---
+++ src/IdeaCard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function IdeaCard() {
+
+    return (
+        <section>
+            <p>Ideas will collect here!</p>
+        </section>
+    )
+}
--- src/IdeaForm.js
+++ src/IdeaForm.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
-
+import IdeaCard from './IdeaCard'
 export default class IdeaForm extends Component {
     constructor() {
         super();
+        this.state = {
+            ideaTitle: "",
+            ideaBodyText: ""
+        }
+    }
+
+    createNewIdea() {
+        
     }
 
     render() {
@@ -13,8 +21,11 @@ export default class IdeaForm extends Component {
                     <input className="user-idea-inputs user-idea-title" type="text" id="form-idea-title"></input>
                     <label for="form-idea-body">Body</label>
                     <textarea className="user-idea-inputs user-idea-body" type="text" id="form-idea-body"></textarea>
-                    <button className="save-idea-button">Save</button>
+                    <button onClick={this.createNewIdea} className="save-idea-button">Save</button>
                 </form>
+                {this.props.ideaCards.length > 0 && (
+                    <IdeaCard />
+                )}
             </section>
         )
     }
