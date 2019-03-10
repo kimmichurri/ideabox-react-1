0a603a5ae3ca2832080ac1b48a18bf86d843e16b
“Kimberly <kimberly.achkio@gmail.com>
2019-03-10 10:23:14 -0600

    Add header component to app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
 import './scss/_App.scss';
+import Header from './Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <p>
-           Make <code>IdeaBoxTriples</code> in React.
-          </p>
-        </header>
+      <Header />
       </div>
     );
   }
---
+++ src/Header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Header() {
+    return (
+        <h1 className='app-name'>IdeaBox<span className='app-name-color-accent'>Triples</span></h1>
+    )
+}
