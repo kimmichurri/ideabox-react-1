3bdf7b3c9280c0fd2aaae405bb52b02ff27d2758
“Kimberly <kimberly.achkio@gmail.com>
2019-03-10 11:54:28 -0600

    Begin idea form styling

--- src/IdeaForm.js
+++ src/IdeaForm.js
@@ -8,11 +8,11 @@ export default class IdeaForm extends Component {
     render() {
         return (
             <section className="idea-form">
-                <form method="get">
+                <form className="idea-inputs" method="get">
                     <label for="form-idea-title">Title</label>
                     <input className="user-idea-inputs user-idea-title" type="text" id="form-idea-title"></input>
                     <label for="form-idea-body">Body</label>
-                    <input className="user-idea-inputs user-idea-body" type="text" id="form-idea-body"></input>
+                    <textarea className="user-idea-inputs user-idea-body" type="text" id="form-idea-body"></textarea>
                     <button className="save-idea-button">Save</button>
                 </form>
             </section>
