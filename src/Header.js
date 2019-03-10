4ef51f98adfcbdc72807d22daf9d616409fcb6b8
“Kimberly <kimberly.achkio@gmail.com>
2019-03-10 10:55:15 -0600

    Move search component to header component

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import Search from './Search';
 
 export default function Header() {
     return (
-        <h1 className='app-name'>IdeaBox<span className='app-name-color-accent'>Triples</span></h1>
+        <div>
+            <h1 className='app-name'>IdeaBox<span className='app-name-color-accent'>Triples</span></h1>
+            <Search />
+        </div>
     )
 }
