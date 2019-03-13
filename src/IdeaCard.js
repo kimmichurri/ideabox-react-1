686779354a63c2c776d2cf299029c76e39ee5029
“Kimberly <kimberly.achkio@gmail.com>
2019-03-13 10:49:48 -0600

    Add preliminary styling to idea card

--- src/IdeaCard.js
+++ src/IdeaCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function IdeaCard({ ideaTitle, ideaBodyText, id, removeIdea}) {
 
     return (
-        <section>
+        <section className="idea-card">
             <h3>{ ideaTitle }</h3>
             <p>{ ideaBodyText }</p>
             <button onClick={()=> removeIdea(id)}>x</button>
